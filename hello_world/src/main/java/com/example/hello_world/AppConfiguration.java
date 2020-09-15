29d08df892fb6ced229e1c7793e3b7a2dd4d0b4e
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-09-16 03:32:11 +0530

    server

--- hello_world/src/main/java/com/example/hello_world/AppConfiguration.java
+++ hello_world/src/main/java/com/example/hello_world/AppConfiguration.java
@@ -1,7 +1,9 @@
 package com.example.hello_world;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class AppConfiguration {
 
     @RequestMapping("/hello")
