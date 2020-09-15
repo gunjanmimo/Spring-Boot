a4e56dca05858001cfd2a6589555a7ac596a446e
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-09-16 04:01:07 +0530

    new

---
+++ dependency_demo/src/main/java/com/example/demo/dependency_demo/Customers.java
@@ -0,0 +1,32 @@
+package com.example.demo.dependency_demo;
+
+public class Customers {
+    private int custId;
+    private String cusrName;
+    private String courseName;
+
+    public int getCustId() {
+        return custId;
+    }
+
+    public void setCustId(int custId) {
+        this.custId = custId;
+    }
+
+    public String getCusrName() {
+        return cusrName;
+    }
+
+    public void setCusrName(String cusrName) {
+        this.cusrName = cusrName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+}
