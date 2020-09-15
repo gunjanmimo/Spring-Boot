0cfbd5fc6235e920adf6c9c8514dcf8b53bb24a0
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-09-16 04:05:56 +0530

    new

--- dependency_demo/src/main/java/com/example/demo/dependency_demo/DependencyDemoApplication.java
+++ dependency_demo/src/main/java/com/example/demo/dependency_demo/DependencyDemoApplication.java
@@ -2,12 +2,14 @@ package com.example.demo.dependency_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DependencyDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependencyDemoApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(DependencyDemoApplication.class, args);
+		Customers c = context.getBean(Customers.class);
 	}
 
 }
