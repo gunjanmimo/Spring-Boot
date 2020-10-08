aa3b7bfc14a1ed59cdd44c4ca7b9b25542af658c
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-10-08 12:47:25 +0530

    new

--- todo project/backend/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/HelloWorldController.java
+++ todo project/backend/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Rest Controller- handle rest request
@@ -15,4 +16,17 @@ public class HelloWorldController {
     public String helloWorld() {
         return "Hello World";
     }
+
+    //hello-world-bean
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    // hello-world/path-variable/gunjan
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean("hello world " + name);
+    }
+
 }
