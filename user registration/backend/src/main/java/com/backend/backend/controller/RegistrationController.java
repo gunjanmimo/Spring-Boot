3e5036a862f652f7b00e32e8f200ca2b00aab7d5
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-10-12 12:51:52 +0530

    new

--- user registration/backend/src/main/java/com/backend/backend/controller/RegistrationController.java
+++ user registration/backend/src/main/java/com/backend/backend/controller/RegistrationController.java
@@ -2,12 +2,25 @@ package com.backend.backend.controller;
 
 
 import com.backend.backend.model.User;
+import com.backend.backend.service.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegistrationController {
-    public void registerUser(@RequestBody User user) {
+    @Autowired
+    private RegistrationService service;
 
+    @GetMapping(path = "/registerUser")
+    public User registerUser(@RequestBody User user) {
+        String tempEmail = user.getEmailId();
+        if (tempEmail != null && !"".equals(tempEmail)) {
+
+        }
+        User userObj = null;
+        userObj = service.saveUser(user);
+        return userObj;
     }
 }
--- user registration/backend/src/main/java/com/backend/backend/service/RegistrationService.java
+++ user registration/backend/src/main/java/com/backend/backend/service/RegistrationService.java
@@ -1,10 +1,22 @@
 package com.backend.backend.service;
 
 
+import com.backend.backend.model.User;
+import com.backend.backend.repo.RegistrationRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class RegistrationService {
+    @Autowired
+    private RegistrationRepo repo; // injected repo
 
+    public User saveUser(User user) {
+        return repo.save(user);
+    }
 
+    public User fetchUserByEmailID(String email) {
+        return repo.findByEmailId(email);
+    }
 }
