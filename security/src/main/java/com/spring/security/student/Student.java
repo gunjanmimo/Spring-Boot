6d59910134078d06d6353b65dead9073f65dcee4
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-10-29 14:31:25 +0530

    new

---
+++ security/src/main/java/com/spring/security/student/Student.java
@@ -0,0 +1,20 @@
+package com.spring.security.student;
+
+public class Student {
+
+    private final Integer id;
+    private final String name;
+
+    public Student(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
